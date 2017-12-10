f19acfdac269061d40bdd8f2bdd518cd0952bb08
spoonm <spoonm@spoonm.org>
2017-12-10 05:03:00 -0200

    added day 4 part 1 in Haskell

---
+++ 2017/hs/4-1.hs
@@ -0,0 +1,13 @@
+import System.IO
+import Control.Applicative
+import qualified Data.Set as Set
+
+validPhrase :: String -> Bool
+validPhrase line = let wordList = words line
+                       wordSet = Set.fromList wordList
+                   in  Set.size wordSet == length wordList
+
+main :: IO ()
+main = do
+    passphrases <- lines <$> getContents
+    print . length . filter validPhrase $ passphrases
