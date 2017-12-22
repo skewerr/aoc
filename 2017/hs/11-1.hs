b9cd1721810ebdbb9e960ffd926b973faef5a5cc
spoonm <spoonm@spoonm.org>
2017-12-22 16:08:12 -0200

    corrected day 11 part 1 in Haskell
    The previous one did work for my provided input, but it was an incorrect
    solution anyways. This one will step through the provided input and
    update values in a map, composing steps as necessary, using a map from
    direction to a tuple of possible compose rules.
    
    That map can be reduced as a list comprehension, however, with which I
    may or may not replace the current hardcoded stuff.

--- 2017/hs/11-1.hs
+++ 2017/hs/11-1.hs
@@ -5,20 +5,31 @@ import qualified Data.Map.Strict as M
 directions :: [String]
 directions = ["n", "ne", "se", "s", "sw", "nw"]
 
-count :: Eq a => a -> [a] -> Int
-count e = length . filter (== e)
+startMap :: M.Map String Int
+startMap = M.fromList . zip directions $ repeat 0
+
+compMap = M.fromList [ ("nw", ("se", ( "s", "sw"), ("ne",  "n")))
+                     , ("ne", ("sw", ( "s", "se"), ("nw",  "n")))
+                     , ( "n", ( "s", ("se", "ne"), ("sw", "nw")))
+                     , ("se", ("nw", ( "n", "ne"), ("sw",  "s")))
+                     , ("sw", ("ne", ( "n", "nw"), ("se",  "s")))
+                     , ( "s", ( "n", ("ne", "se"), ("nw", "sw"))) ]
+
+distance :: M.Map String Int -> Int
+distance = sum
+
+updateSteps :: String -> M.Map String Int -> M.Map String Int
+updateSteps k m
+    | m M.!  z > 0 = dec z m
+    | m M.! d1 > 0 = dec d1 . inc i1 $ m
+    | m M.! d2 > 0 = dec d2 . inc i2 $ m
+    |    otherwise = inc k m
+    where
+        (z, (d1, i1), (d2, i2)) = compMap M.! k
+        dec k = M.insertWith (flip (-)) k 1
+        inc k = M.insertWith (+) k 1
 
 main :: IO ()
 main = do
-    steps <- splitOn "," <$> getLine
-    let
-        stepsMap = M.fromList [(d,c) | d <- directions, let c = count d steps]
-        northCnv = min (stepsMap M.! "ne") (stepsMap M.! "nw")
-        southCnv = min (stepsMap M.! "se") (stepsMap M.! "sw")
-        diagWeEa = abs $ stepsMap M.! "nw" - stepsMap M.! "se"
-        diagEaWe = abs $ stepsMap M.! "ne" - stepsMap M.! "sw"
-        northNew = stepsMap M.! "n" + northCnv
-        southNew = stepsMap M.! "s" + southCnv
-        vertWalk = abs $ northNew - southNew
-        diagWalk = abs $ diagWeEa - diagEaWe
-    print $ vertWalk + diagWalk
+    stepsMap <- foldl (flip updateSteps) startMap <$> splitOn "," <$> getLine
+    print $ distance stepsMap
